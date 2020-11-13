5d34acabf48276dcf54c03657e4edf5907a74774
Alexandru Cristian Ion <AlexandruCristian.Ion@endava.com>
2020-11-13 17:34:45 +0200

    Added index file

---
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tasker.Pages
+{
+    public class CreateTask : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
