1a2bbb152a0213042041ffadda8b1972be9081f8
Alexandru Cristian Ion <AlexandruCristian.Ion@endava.com>
2020-11-13 17:47:01 +0200

    Added Task Class

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
     public class CreateTask : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
         public void OnGet()
         {
             
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
